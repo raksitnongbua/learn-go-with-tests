37c31ac9b3503a9395090458b2b0cad7cd3b747f
Tan Raksit <tan.raksit@bitkub.com>
2024-01-27 20:56:48 +0700

    feat: :white_check_mark: complete Hello, World section

---
+++ hello_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
